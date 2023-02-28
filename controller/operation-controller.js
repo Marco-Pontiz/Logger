fb40dbfe223564c2fc919e0ae763cf5afa9da65e
Marco Pontiz <marcopontiz@gmail.com>
2023-02-28 04:37:50 -0300

    Implementando capas parte 2

---
+++ controller/operation-controller.js
@@ -0,0 +1,31 @@
+import { sum, substract, multiply, divide, getAll } from '../busisness/operation-business';
+
+const suma0ps = async(req, res) => {
+    let {a,b} = req.query;
+    res.send(`La suma de ${a} y ${b} es ${await sum (Number(a), Number(b))}`);
+}
+
+const resta0ps = async(req, res) => {
+    let {a,b} = req.query;
+    res.send(`La resta de ${a} y ${b} es ${await substract (Number(a), Number(b))}`);
+}
+
+const multiplicar0ps = async(req, res) => {
+    let {a,b} = req.query;
+    res.send(`La multiplicacion de ${a} y ${b} es ${await multiply (Number(a), Number(b))}`);
+}
+
+const dividir0ps = async(req, res) => {
+    let {a,b} = req.query;
+    res.send(`La division de ${a} y ${b} es ${await divide (Number(a), Number(b))}`);
+}
+
+const getAll0ps = async (req,res) => res.send(await getAll());
+
+export {
+    suma0ps,
+    resta0ps,
+    multiplicar0ps,
+    dividir0ps,
+    getAll0ps
+};
