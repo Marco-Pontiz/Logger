e0b54fd6650b355b371dbe2bb9b2e53b7e266dcd
Marco Pontiz <marcopontiz@gmail.com>
2023-02-28 14:58:34 -0300

    Implementando capas parte 5

--- busisness/operation-business.js
+++ busisness/operation-business.js
@@ -4,23 +4,26 @@ import {save, getAll as getAll0ps} from '../persistence/operadores-persistentes'
 async function sum (a,b) {
     const result0p = suma (a, b);
     await save({ params: [a, b] , result: result0p, operation: 'suma', tiemstamp: new Date.now ()});
-    return result;
+    return result0p;
 };
 
 async function substract (a, b) {
     const result0p = resta(a, b);
     await save({ params: [a, b] , result: result0p, operation:'restar', tiemstamp: new Date.now ()});
+    return result0p;
 }
 
 
 async function multiply(a,b) {
     const result0p = mult(a, b);
     await save({params: [a, b] , result: result0p, operation:'multiplicación', tiemstamp: new Date.now ()});
+    return result0p;
 };
 
 async function divide(a, b) {
     const result0p = div(a, b);
     await save({params: [a, b] , result: result0p, operation:'dividir', tiemstamp: new Date.now ()});
+    return result0p;
 };
 
 async function listAll() {
