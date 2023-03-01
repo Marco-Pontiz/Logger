c8fb60a50a0f58ec88d3ef12502fc18ebdfe6265
Marco Pontiz <marcopontiz@gmail.com>
2023-03-01 03:25:02 -0300

    mplementando capas Parte 8

--- persistence/operadores-persistentes.js
+++ persistence/operadores-persistentes.js
@@ -1,10 +1,47 @@
-const operations = [];
+import MemoryContainer from "./container/memory.container";
+import FileConteiner from "./container/file.container";
 
-const save = (obj) => operations.push(obj)
+const containerMode = 'memory';
+let container;
 
-const getAll = () => operations; 
+switch (containerMode) {
+    case 'fs':
+        container = new FileContainer('./persistence/container/operaciones.json');
+        break;
+        default:
+            container = new MemoryContainer();
+
+}
+
+async function save (obj) { 
+    return await container.save(obj)
+};
+
+async function getAll() {
+    return await container.listAll();
+} 
+
+async function getById(id) {
+    return await container.list(id);
+}
+
+async function update(obj) {
+    return await container.update(obj);
+}
+
+async function deleteById(id) {
+    return await container.delete(id);
+}
+
+async function deleteAll() {
+    return await container.deleteAll();
+}
 
 export {
     save, 
-    getAll
+    getAll,
+    getById,
+    update,
+    deleteById,
+    deleteAll
 };
