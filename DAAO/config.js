ddbbfe3057942ffecd80eb5f2eccc1d021b876b4
Marco Pontiz <marcopontiz@gmail.com>
2023-03-04 05:48:40 -0300

    Agregando DAAO

---
+++ DAAO/config.js
@@ -0,0 +1,10 @@
+const Config = {
+    db: {
+        name: 'ecommerce',
+        collection: 'productos',
+        cnxStr: 'mongodb://localhost:27017/',
+        projection: {__v:0}
+    }
+}
+
+export default Config
