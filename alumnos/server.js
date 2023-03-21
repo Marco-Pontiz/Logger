8a287fca4d939f8a61ce84202e2c92cc1aeadf7f
Marco Pontiz <marcopontiz@gmail.com>
2023-03-21 06:04:10 -0300

    alumnos concluida

---
+++ alumnos/server.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const KoaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(KoaBody());
+
+const alumnos = require('./alumnos.js');
+
+app.use(alumnos.routes());
+
+const port = 8080;
+const server = app.listen(port, () => {
+    console.log(`Server online PORT:${port}`);
+});
+
+server.on('error', error => console.log(`Server error`, error));
